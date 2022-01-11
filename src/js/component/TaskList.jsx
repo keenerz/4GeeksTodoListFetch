ec2bd50a755890c102bea44fcddf90d68240c228
Keenan <kkan68@gmail.com>
2022-01-11 03:15:22 +0000

    needs visual overhaul

---
+++ src/js/component/TaskList.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+
+const TaskList = () => {
+	let [task, setTask] = useState("");
+	let [list, setList] = useState([]);
+
+	const handleInput = (e) => {
+		if (e.keyCode == 13) {
+			setTask(e.target.value);
+			setList([...list, task]);
+			setTask("");
+		}
+	};
+
+	return (
+		<div className="d-inline justify-content-center">
+			<div className="input-group border">
+				<input
+					className="form-control"
+					type="text"
+					placeholder="To Do List"
+					onChange={(event) => setTask(event.target.value)}
+					onKeyDown={(e) => handleInput(e)}
+				/>
+			</div>
+			<div className="w-100 h-100">
+				<ul>
+					{list.map((singleTask, i) => {
+						return (
+							<li
+								className="d-flex justify-content-between ps-5 pe-4 py-2 border-end border-start border-bottom text-muted fw-light fs-5"
+								key={i}>
+								{singleTask}
+							</li>
+						);
+					})}
+				</ul>
+				<div className="ps-5 py-1 border-end border-start border-bottom text-muted fw-light fs-6 text-start">
+					<span>{list.length} Items</span>
+				</div>
+			</div>
+		</div>
+	);
+};
+export default TaskList;
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import TaskList from "./TaskList.jsx";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container card">
+			<div className="pt-5 text-center mx-auto">
+				<h1 className="fw-light title opacity-25">todos</h1>
+			</div>
+			<div className="row px-5 d-flex justify-content-center card-body">
+				<div className="text-center d-inline justify-content-center">
+					<TaskList />
+				</div>
+			</div>
 		</div>
 	);
 };
